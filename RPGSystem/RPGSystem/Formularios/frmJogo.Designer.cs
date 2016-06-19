2c40d9de664e4d4c7aaeef4a5815b6e206a36361
Melysugar <melissamelo_@hotmail.com>
2016-06-18 22:01:11 -0300

    new commit

---
+++ RPGSystem/RPGSystem/Formularios/frmJogo.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace RPGSystem.Formularios
+{
+    partial class frmJogo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.inventárioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inventárioToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.carregarÚltimoSaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarESairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tcJogo = new System.Windows.Forms.TabControl();
+            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comoJogarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusToolStripMenuItem,
+            this.inventárioToolStripMenuItem,
+            this.ajudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(919, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // inventárioToolStripMenuItem
+            // 
+            this.inventárioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inventárioToolStripMenuItem1,
+            this.statusToolStripMenuItem1});
+            this.inventárioToolStripMenuItem.Name = "inventárioToolStripMenuItem";
+            this.inventárioToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.inventárioToolStripMenuItem.Text = "Jogador";
+            // 
+            // statusToolStripMenuItem
+            // 
+            this.statusToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salvarToolStripMenuItem1,
+            this.carregarÚltimoSaveToolStripMenuItem,
+            this.salvarESairToolStripMenuItem});
+            this.statusToolStripMenuItem.Name = "statusToolStripMenuItem";
+            this.statusToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.statusToolStripMenuItem.Text = "Opções";
+            // 
+            // inventárioToolStripMenuItem1
+            // 
+            this.inventárioToolStripMenuItem1.Name = "inventárioToolStripMenuItem1";
+            this.inventárioToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.inventárioToolStripMenuItem1.Text = "Inventário";
+            // 
+            // statusToolStripMenuItem1
+            // 
+            this.statusToolStripMenuItem1.Name = "statusToolStripMenuItem1";
+            this.statusToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.statusToolStripMenuItem1.Text = "Status";
+            // 
+            // salvarToolStripMenuItem1
+            // 
+            this.salvarToolStripMenuItem1.Name = "salvarToolStripMenuItem1";
+            this.salvarToolStripMenuItem1.Size = new System.Drawing.Size(183, 22);
+            this.salvarToolStripMenuItem1.Text = "Salvar";
+            // 
+            // carregarÚltimoSaveToolStripMenuItem
+            // 
+            this.carregarÚltimoSaveToolStripMenuItem.Name = "carregarÚltimoSaveToolStripMenuItem";
+            this.carregarÚltimoSaveToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.carregarÚltimoSaveToolStripMenuItem.Text = "Carregar último save";
+            // 
+            // salvarESairToolStripMenuItem
+            // 
+            this.salvarESairToolStripMenuItem.Name = "salvarESairToolStripMenuItem";
+            this.salvarESairToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.salvarESairToolStripMenuItem.Text = "Salvar e sair";
+            // 
+            // tcJogo
+            // 
+            this.tcJogo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcJogo.Location = new System.Drawing.Point(0, 24);
+            this.tcJogo.Name = "tcJogo";
+            this.tcJogo.SelectedIndex = 0;
+            this.tcJogo.Size = new System.Drawing.Size(919, 492);
+            this.tcJogo.TabIndex = 2;
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            this.ajudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.comoJogarToolStripMenuItem});
+            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.ajudaToolStripMenuItem.Text = "Ajuda";
+            // 
+            // comoJogarToolStripMenuItem
+            // 
+            this.comoJogarToolStripMenuItem.Name = "comoJogarToolStripMenuItem";
+            this.comoJogarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.comoJogarToolStripMenuItem.Text = "Como jogar";
+            // 
+            // frmJogo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 516);
+            this.Controls.Add(this.tcJogo);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmJogo";
+            this.Text = "Ultimate RPG";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem statusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem carregarÚltimoSaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarESairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inventárioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inventárioToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem statusToolStripMenuItem1;
+        public System.Windows.Forms.TabControl tcJogo;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comoJogarToolStripMenuItem;
+    }
+}
---
+++ RPGSystem/RPGSystem/Formularios/ucJogarLuta.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace RPGSystem.Formularios
+{
+    partial class ucJogarLuta
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTrocarArma = new System.Windows.Forms.Button();
+            this.btnAtacar = new System.Windows.Forms.Button();
+            this.btnDefender = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxVilao = new System.Windows.Forms.TextBox();
+            this.tbxJogador = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnTrocarArma
+            // 
+            this.btnTrocarArma.Font = new System.Drawing.Font("Minion Pro", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTrocarArma.Location = new System.Drawing.Point(137, 322);
+            this.btnTrocarArma.Name = "btnTrocarArma";
+            this.btnTrocarArma.Size = new System.Drawing.Size(216, 42);
+            this.btnTrocarArma.TabIndex = 0;
+            this.btnTrocarArma.Text = "Trocar Arma";
+            this.btnTrocarArma.UseVisualStyleBackColor = true;
+            // 
+            // btnAtacar
+            // 
+            this.btnAtacar.Font = new System.Drawing.Font("Minion Pro", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtacar.Location = new System.Drawing.Point(80, 263);
+            this.btnAtacar.Name = "btnAtacar";
+            this.btnAtacar.Size = new System.Drawing.Size(130, 42);
+            this.btnAtacar.TabIndex = 0;
+            this.btnAtacar.Text = "Atacar";
+            this.btnAtacar.UseVisualStyleBackColor = true;
+            // 
+            // btnDefender
+            // 
+            this.btnDefender.Font = new System.Drawing.Font("Minion Pro", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDefender.Location = new System.Drawing.Point(285, 263);
+            this.btnDefender.Name = "btnDefender";
+            this.btnDefender.Size = new System.Drawing.Size(130, 42);
+            this.btnDefender.TabIndex = 0;
+            this.btnDefender.Text = "Defender";
+            this.btnDefender.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vida";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Arma Atual";
+            // 
+            // tbxVilao
+            // 
+            this.tbxVilao.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.tbxVilao.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxVilao.Location = new System.Drawing.Point(36, 26);
+            this.tbxVilao.Name = "tbxVilao";
+            this.tbxVilao.Size = new System.Drawing.Size(100, 24);
+            this.tbxVilao.TabIndex = 2;
+            this.tbxVilao.Text = "Enemy";
+            // 
+            // tbxJogador
+            // 
+            this.tbxJogador.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.tbxJogador.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxJogador.Location = new System.Drawing.Point(36, 161);
+            this.tbxJogador.Name = "tbxJogador";
+            this.tbxJogador.Size = new System.Drawing.Size(100, 24);
+            this.tbxJogador.TabIndex = 2;
+            this.tbxJogador.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Vida";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(33, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Arma Atual";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(80, 199);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(111, 230);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // ucJogarLuta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbxJogador);
+            this.Controls.Add(this.tbxVilao);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDefender);
+            this.Controls.Add(this.btnAtacar);
+            this.Controls.Add(this.btnTrocarArma);
+            this.Name = "ucJogarLuta";
+            this.Size = new System.Drawing.Size(493, 383);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTrocarArma;
+        private System.Windows.Forms.Button btnAtacar;
+        private System.Windows.Forms.Button btnDefender;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxVilao;
+        private System.Windows.Forms.TextBox tbxJogador;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
